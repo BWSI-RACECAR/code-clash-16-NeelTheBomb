3e05f5bd4a46d7c01cef9416eb282d06832f04b4
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-08-02 09:18:37 -0400

    Update isracecarbounded.py

--- isracecarbounded.py
+++ isracecarbounded.py
@@ -8,34 +8,35 @@ class Solution:
             cur_dir=2
             angle = 0
             print(instructions)
-            print(instructions.count("R") == instructions.count("L"))
-            return instructions.count("R") == instructions.count("L")
-            # for b in range(3):
-            #     for i in range(len(instructions)):
-            #         if angle == 0:
-            #             if instructions[i] == "G":
-            #                 cur_pos = [cur_pos[0], cur_pos[1] + 1]
-            #         if angle == 90:
-            #             if instructions[i] == "G":
-            #                 cur_pos = [cur_pos[0] + 1, cur_pos[1]]
-            #         if angle == 180:
-            #             if instructions[i] == "G":
-            #                 cur_pos = [cur_pos[0], cur_pos[1]  - 1]
-            #         if angle == 270:
-            #             if instructions[i] == "G":
-            #                 cur_pos = [cur_pos[0] - 1, cur_pos[1]]
-            #         if angle == 360:
-            #             if instructions[i] == "G":
-            #                 cur_pos = [cur_pos[0], cur_pos[1] + 1]
-            #         if instructions[i] == "L":
-            #             angle = angle + 90
-            #         if instructions[i] == "R":
-            #             angle = angle - 90
-            #         if b > 0:
-            #             if cur_pos == initial_pos:
-            #                 return True
-            #             else:
-            #                 return False
+            # return(instructions.count("R") != instructions.count("L"))
+            for b in range(3):
+                for i in range(len(instructions)):
+                    if angle == 0:
+                        if instructions[i] == "G":
+                            cur_pos = [cur_pos[0], cur_pos[1] + 1]
+                    if angle == 90:
+                        if instructions[i] == "G":
+                            cur_pos = [cur_pos[0] + 1, cur_pos[1]]
+                    if angle == 180:
+                        if instructions[i] == "G":
+                            cur_pos = [cur_pos[0], cur_pos[1]  - 1]
+                    if angle == 270:
+                        if instructions[i] == "G":
+                            cur_pos = [cur_pos[0] - 1, cur_pos[1]]
+                    if angle == 360:
+                        if instructions[i] == "G":
+                            cur_pos = [cur_pos[0], cur_pos[1] + 1]
+                    if angle == 360:
+                        angle = 0
+                    if instructions[i] == "L":
+                        angle = angle + 90
+                    if instructions[i] == "R":
+                        angle = angle - 90
+                    if b > 0:
+                        if cur_pos == initial_pos:
+                            return True
+                        else:
+                            return False
             
             #TODO: Write code below to returnn a boolean value with the solution to the prompt.
             pass
